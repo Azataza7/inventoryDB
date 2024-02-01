83e40fe29c093714c605bb102cc7525944f4be61
Azat Kulbaev <kulbaevaazat@gmail.com>
2024-02-01 23:07:00 +0600

    Initial commit #1 created new api project(express)

---
+++ config.ts
@@ -0,0 +1,16 @@
+import path from "path";
+
+const rootPath = __dirname;
+
+const config = {
+    rootPath,
+    publicPath: path.join(rootPath, 'public'),
+    mysql: {
+        host: 'localhost',
+        user: 'root',
+        database: '',
+        password: '',
+    }
+}
+
+export default config;
